b14d152cfb080cbcb019522d4c635c4695a31d63
Rodrigo Martins <rooxalot@gmail.com>
2020-06-10 00:35:04 -0300

    Criado serviço para resetar a senha após recebimento de token

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -3,7 +3,7 @@ import { uuid } from 'uuidv4';
 import IUserTokensRepository from '../IUserTokensRepository';
 
 export default class FakeUserTokensRepository implements IUserTokensRepository {
-  private tokens: UserToken[] = [];
+  tokens: UserToken[] = [];
 
   public async generate(userID: string): Promise<UserToken> {
     const userToken = new UserToken();
@@ -15,6 +15,12 @@ export default class FakeUserTokensRepository implements IUserTokensRepository {
     });
 
     this.tokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.tokens.find((t) => t.token === token);
 
     return userToken;
   }
